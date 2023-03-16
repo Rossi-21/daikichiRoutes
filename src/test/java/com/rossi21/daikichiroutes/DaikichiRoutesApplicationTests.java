c2be6ec808cb87766e1d7a362b03ac807432dcac
Rossi21 <rossi21@gmail.com>
2023-03-16 06:49:31 -0700

    first commit

---
+++ src/test/java/com/rossi21/daikichiroutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossi21.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
