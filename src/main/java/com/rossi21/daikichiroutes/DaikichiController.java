e38aa159538d0d6b16e0712b083c0702c5b32a8f
Rossi21 <rossi21@gmail.com>
2023-03-17 07:43:07 -0700

    Second Commit

--- src/main/java/com/rossi21/daikichiroutes/DaikichiController.java
+++ src/main/java/com/rossi21/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.rossi21.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,18 @@ public class DaikichiController {
         @RequestMapping("/tomorrow")
         public String tomorrow() {
                 return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+        }
+        @RequestMapping("/travel/{place}")
+        public String showPlace(@PathVariable("place") String place)
+        	{return "Congratulations! You will soon travel to " + place + ".";}
+        @RequestMapping("/lotto/{num}")
+        public String showNum(@PathVariable("num") int num) {
+        	if(num % 2 == 0){
+        		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+        	}
+        	else{
+        		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        	} 
         }
 
 }
